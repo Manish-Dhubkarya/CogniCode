c1fc9664bd6060fb5bb524c209a95b45b640ed43
Manish Dhubkarya <147823856+Manish-Dhubkarya@users.noreply.github.com>
2025-05-16 12:19:30 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/CogniCode_Website/', // <-- ✅ this is required
   plugins: [
     react(),
     tailwindcss(),
