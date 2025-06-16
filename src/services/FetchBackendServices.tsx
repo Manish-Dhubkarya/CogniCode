d6ade098f9c06977a3741c9e611b706e317b3b21
Manish Dhubkarya <manishdhubkarya@gmail.com>
2025-06-17 00:42:51 +0530

    server url change

--- src/services/FetchBackendServices.tsx
+++ src/services/FetchBackendServices.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const serverURL = "http://localhost:4000"
+const serverURL = "https://cognicode-backend.onrender.com"
 const postData = async (url:any, body:any) => {
     try {
         const response = await axios.post(`${serverURL}/${url}`, body)
